269e9a0a66121ad7e9ce9ac74ea4cd916640a358
James Morris <jamesamorris@yahoo.com>
2019-11-15 17:01:12 -0500

    FInish flow

--- flow.py
+++ flow.py
@@ -8,6 +8,7 @@ class Node():
 	def getAvailableEdges(self, ignore=[]):
 		return [e for e in self.edges if e[1] > 0 and not(e[0] in ignore)]
 	def addEdge(self, edge):
+		if(edge[0] >= 0 and edge[1] >= 0 and edge[2] >= 0)
 		self.edges.append(edge)
 	def setEdge(self, edge):
 		for i,e in enumerate(self.edges):
@@ -59,18 +60,9 @@ def fordFulkerson(vertexNum, edges, source, sink):
 		for src, dst in zip(path[:-1], path[1:]):
 			residual[src].setEdgeWeight(dst, minWeight, diff=True)
 			residual[dst].setEdgeWeight(src, -minWeight, diff=True)
-		try:
 			targetResults = targetedBFS(residual, source, sink)
-		except:
-			pass
+
+
+
+	[print(x) for x in residual]
 	return flow
-
-
-edges = [(4, 3, 3), (1, 2, 6), (2, 4, 3)]
-# nodes = createGraph(5, edges)
-# backflow = createGraph(5, edges, reverse=True)
-# [print(x) for x in backflow]
-#
-# print(targetedBFS(nodes, 1, 4))
-
-print(fordFulkerson(5, edges, 1, 4))
